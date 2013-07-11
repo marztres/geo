26e863f3e7be1da613e16564427bb6289e12909b
Marz13 <marz13@outlook.com>
2013-07-11 14:49:27 -0500

    Corregido db

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	define( "DB_HOST", "localhost" );
 	define( "DB_USER", "root" );
-	define( "DB_PASS", "" );
+	define( "DB_PASS", "root" );
 	define( "DB_NAME", "geotecnia" );
 	define( "DB_PREFIX", "" );
 ?>
